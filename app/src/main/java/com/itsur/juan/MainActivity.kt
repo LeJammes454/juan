f28c8cfc22e01b235c7c5d15b02a51ae67506295
LeJammes454 <98853831+LeJammes454@users.noreply.github.com>
2022-12-08 15:22:29 -0600

    Prueba uno

---
+++ app/src/main/java/com/itsur/juan/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.itsur.juan
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.itsur.juan.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        binding.juanito.setOnClickListener {
+
+            
+        }
+    }
+
+
+
+}
